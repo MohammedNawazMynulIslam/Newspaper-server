6a743ccefd0c8e813655e642817e1c4ac3c3d2fe
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-26 12:22:06 +0600

    added single article api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const app = express()
 const cors=require('cors')
 const port = process.env.PORT || 3000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jyxnen.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 app.use(cors())
@@ -35,10 +35,40 @@ app.post('/article', async(req,res)=>{
     const result = await articleCollection.insertOne(article)
     res.send(result)
 })
+
+app.get('/article/:id',async(req,res)=>{
+const id = req.params.id
+const result = await articleCollection.findOne({_id:new ObjectId(id)})
+res.send(result)
+});
+
 app.get('/article',async(req,res)=>{
-    const result = await articleCollection.find().toArray()
-    res.send(result)
+  try{
+    const {search,publisher,tags}=req.query;
+    // console.log('Search:', search);
+    // console.log('Publisher:', publisher);
+    // console.log('Tags:', tags);
+    let filter = {};
+    if (search) {
+      filter.title = { $regex: new RegExp(search, 'i') };
+    }
+    if (publisher) {
+      filter.publisher = publisher;
+    }
+    if (tags) {
+      filter.tags = { $in: Array.isArray(tags) ? tags : [tags] };
+    }
+
+    // console.log('Filter:', filter);
+    const result = await articleCollection.find().toArray();
+    // console.log("mongodb query",result);
+    res.send(result);
+}catch(error){
+  console.error("error fetching",error);
+  res.status(500).json({error:"internal server error"})
+}
 })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
